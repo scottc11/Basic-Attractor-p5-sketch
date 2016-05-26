abd9a3085e30a321876e69599a08cf83caa200aa
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-05-26 17:01:31 -0400

    added a range to the attractor class

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var gravConst = 1;
+var attractorRange = 50;
 var obj1;
 var obj2;
 var attractor;
@@ -46,6 +47,7 @@ function draw() {
 //MOVER CLASS
 function MoverObject(_locX, _locY, _size) {
   this.location = new p5.Vector(_locX, _locY);
+  this.origin = new p5.Vector(_locX, _locY);
   this.velocity = new p5.Vector(0.0, 0.0);
   this.size = _size;
 
@@ -68,8 +70,12 @@ function MoverObject(_locX, _locY, _size) {
 function Attractor(_locX, _locY, _size) {
   this.location = new p5.Vector(_locX, _locY);
   this.size = _size;
+  this.range = 100;
 
   this.draw = function() {
+    fill(77);
+    stroke(255);
+    ellipse(this.location.x, this.location.y, this.range*2, this.range*2);
     stroke(255);
     fill(44);
     smooth();
@@ -78,16 +84,26 @@ function Attractor(_locX, _locY, _size) {
 
   // Returns a Vector to be applied to the MoverObject location/veleocity
   this.attract = function(obj) {
+
+    // calculations for objects distance
     var forceDirection = p5.Vector.sub(this.location, obj.location);
     var distance = forceDirection.mag();
-    distance = constrain(distance, 40, 300);
 
 
-    forceDirection.normalize();
-    // below is the equation for calculating the gravitational force of an object
-    var magnitude = (gravConst * this.size * obj.size) / (distance * distance);
-    var force = forceDirection.mult(magnitude);
-    return force;  // p5.Vector
+    //attract object if it is in given range (attractor.range)
+    if (distance < this.range) {
+      // the constrain() method will prevent unwanted effects when the object gets to close to the center of the attractor.
+      distance = constrain(distance, 40, 300);
+
+      // unsure what the pupurpose of normalize is, but it turns a vector into a number between 0.0 and 1.0 - kinda like map()
+      forceDirection.normalize();
+
+      // below is the equation for calculating the gravitational force of an object
+      var magnitude = (gravConst * this.size * obj.size) / (distance * distance);
+      var force = forceDirection.mult(magnitude);
+      return force;  // p5.Vector
+    }
+
   }
 
 }
